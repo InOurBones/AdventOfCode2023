55a4470a3e3a5649e71773fc226c3c9737495dfb
Tom Lagerweij <tom.lagerweij@mccoy-partners.com>
2023-12-12 19:13:27 +0100

    more ts

--- typescript/05/star_two.ts
+++ typescript/05/star_two.ts
@@ -1,26 +1,10 @@
 import * as fs from "fs/promises";
 import * as path from "path";
 
-function lower(l1: number[], l2: number[]) {
-    const minValue = Math.min(l1[0], l2[0])
-    const maxValue = Math.max(l1[0], l2[0])
-    return maxValue > minValue ? [minValue, maxValue] : []
-}
-
-function intersect(l1: number[], l2: number[]) {
+function findIntersect(l1: number[], l2: number[]) {
     const minIntersect = Math.max(l1[0], l2[0])
     const maxIntersect = Math.min(l1[1], l2[1])
     return minIntersect <= maxIntersect ? [minIntersect, maxIntersect] : []
-}
-
-function upper(l1: number[], l2: number[]) {
-    const minValue = Math.min(l1[1], l2[1]) + 1
-    const maxValue = Math.max(l1[1], l2[1])
-    return minValue < maxValue ? [minValue, maxValue] : []
-}
-
-function divide(l1: number[], l2: number[]): number[][] {
-    return [lower(l1, l2), intersect(l1, l2), upper(l1, l2)]
 }
 
 function processMapSeeds(map: [number[], number][], seeds: number[][]): number[][] {
@@ -32,7 +16,7 @@ function processMapSeeds(map: [number[], number][], seeds: number[][]): number[]
 
         for (let i = 0; i < map.length; i++) {
             const [range, dst]: [number[], number] = map[i];
-            const [lower, intersect, upper] = divide(item, range)
+            const intersect = findIntersect(item, range)
             if (intersect.length == 0)
                 continue
     
@@ -42,11 +26,6 @@ function processMapSeeds(map: [number[], number][], seeds: number[][]): number[]
     
             if (intersect[0] == item[0] && intersect[1] == item[1])
                 break
-
-            if (upper.length == 0 && upper[1] == item[1])
-                seeds.push(upper)
-            else if (lower.length == 0 && lower[0] == item[0])
-                seeds.push(lower)
         }
 
         if (startingLength == ranges.length)
@@ -63,7 +42,6 @@ function processMapSeeds(map: [number[], number][], seeds: number[][]): number[]
     const [seeds, ...maps] = arr;
     const seedsArr = seeds.split(": ")[1].split(" ").map(Number)
 
-    // load starting ranges
     let seedRanges: number[][] = []
     for (let i = 0; i < seedsArr.length; i+=2) {
         seedRanges.push([seedsArr[i], seedsArr[i] + seedsArr[i + 1] - 2])
